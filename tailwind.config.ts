d69459c45fb1023b891b7f30277de0bbf107d176
Ushna-azhar <773.ushnaazhar@gmail.com>
2025-01-18 05:11:43 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
       },
       fontFamily: {
         satoshi: ['Satoshi', 'sans-serif'], // Satoshi font added
+         'integral': ['"Integral CF"', 'sans-serif'],
       },
     },
   },
